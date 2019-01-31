46f1e471af0cbc3373f99bada17c9c7bdc30d9d2
Isaac Fonseca <isaac@launchxd.com>
2019-01-31 11:21:24 -0500

    added .reduce to display warning to please add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // transforms the ingredient object into an array 
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+     })
+    //  transforms array into something else
+     .reduce((arry, el) => {
+         return arry.concat(el)
+     }, []);
+     if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+     }
+     console.log(transformedIngredients)
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />            
-            <BurgerIngredient type='meat' />            
-            <BurgerIngredient type='cheese' />            
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />            
         </div>
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,21 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor and super can be used as well, but as we see below this is more modern and less syntax
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
+    }
+    
     render () {
         return (
             <Aux>
-                <Burger />
+                {/* this.state.ingredients passes done state stuff which is at the top of the class component */}
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
 
             </Aux>
